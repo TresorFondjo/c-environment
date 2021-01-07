3d15c48381c5d9c16972f44fe184d623c826c4a7
Valentin Rousseau <valentin.rousseau@edu.devinci.fr>
2021-01-07 20:39:07 +0100

    nom du programme

---
+++ enumerate_numbers/README.md
@@ -0,0 +1,7 @@
+Projet OS 
+08/01/2021
+
+Trésor
+Yanis
+Valentin
+Emmanuel
---
+++ enumerate_numbers/main.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main(){
+
+	for(int x = 0; x<=10; x++){
+		printf("%d\n",x);}
+	return 0;
+}
